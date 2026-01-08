d47c4f4cbacf534a6e5ec855967e1606edc51ee6
Hanbin-GW <hanbin.scpsl@gmail.com>
2026-01-09 00:58:05 +0900

    initial setup

--- sjajejuvex/settings.py
+++ sjajejuvex/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#0g!m2y4&1sk@lq)lzu(-ipyq#i+sabb%$=q1btfrx5sz63edu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'sjajejuvex.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
